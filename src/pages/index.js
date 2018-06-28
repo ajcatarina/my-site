6969ca83f911fbc7bc6a85cd09da0158e9f506bb
AJ <ajcatarina@gmail.com>
2018-06-28 23:39:02 +0800

    design

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,31 @@ const IndexPage = () => (
         >
       AJ CATARINA
     </h1>
+    <div>
     <div style={{ height: '160px',
-                  width: '400px',
+                  maxWidth: '400px',
                   padding: '10px 25px',
                   border: '2px solid white',
                   color: 'white',
                   textAlign: 'right',
                   fontFamily: 'Oswald',
                   fontSize: '80px',
+                  float: 'left',
                 }} >
                 DESIGNER
+    </div>
+    <div style={{ height: '160px',
+                  maxWidth: '400px',
+                  padding: '10px 25px',
+                  border: '2px solid #f93b8e',
+                  color: '#f93b8e',
+                  textAlign: 'right',
+                  fontFamily: 'Oswald',
+                  fontSize: '80px',
+                  float: 'right',
+                }} >
+                DEVELOPER
+    </div>
     </div>
   </div>
 )
