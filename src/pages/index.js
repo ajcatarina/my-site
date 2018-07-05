fd4bc26d9625674593e58c46cc101e868008c271
AJ <ajcatarina@gmail.com>
2018-07-06 01:04:25 +0800

    fixed homepage mobile view

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import MediaQuery from 'react-responsive'
 
 const IndexPage = () => (
   <div>
@@ -7,32 +8,61 @@ const IndexPage = () => (
         >
       AJ CATARINA
     </h1>
-    <div>
-    <div style={{ height: '160px',
-                  maxWidth: '400px',
-                  padding: '10px 25px',
-                  border: '2px solid white',
-                  color: 'white',
-                  textAlign: 'right',
-                  fontFamily: 'Oswald',
-                  fontSize: '80px',
-                  float: 'left',
-                }} >
-                DESIGNER
-    </div>
-    <div style={{ height: '160px',
-                  maxWidth: '400px',
-                  padding: '10px 25px',
-                  border: '2px solid #f93b8e',
-                  color: '#f93b8e',
-                  textAlign: 'right',
-                  fontFamily: 'Oswald',
-                  fontSize: '80px',
-                  float: 'right',
-                }} >
-                DEVELOPER
-    </div>
-    </div>
+
+    <MediaQuery maxDeviceWidth={480}>
+      {(matches) => {
+        if (!matches) {
+          return <div>
+                    <div style={{ maxWidth: '400px',
+                                  padding: '10px 25px',
+                                  border: '2px solid white',
+                                  color: 'white',
+                                  textAlign: 'right',
+                                  fontFamily: 'Oswald',
+                                  fontSize: '80px',
+                                  float: 'left',
+                                }} >
+                                DESIGNER
+                    </div>
+                    <div style={{ maxWidth: '400px',
+                                  padding: '10px 25px',
+                                  border: '2px solid #f93b8e',
+                                  color: '#f93b8e',
+                                  textAlign: 'right',
+                                  fontFamily: 'Oswald',
+                                  fontSize: '80px',
+                                  float: 'right',
+                                }} >
+                                DEVELOPER
+                    </div>
+                  </div>
+        } else {
+          return <div>
+                    <div style={{ maxWidth: '400px',
+                                  padding: '10px 25px',
+                                  border: '2px solid white',
+                                  color: 'white',
+                                  textAlign: 'center',
+                                  fontFamily: 'Oswald',
+                                  fontSize: '55px',
+                                }} >
+                                DESIGNER
+                    </div>
+                    <div style={{ maxWidth: '400px',
+                                  padding: '10px 25px',
+                                  border: '2px solid #f93b8e',
+                                  color: '#f93b8e',
+                                  textAlign: 'center',
+                                  fontFamily: 'Oswald',
+                                  fontSize: '55px',
+                                }} >
+                                DEVELOPER
+                    </div>
+                  </div>
+        }
+      }}
+    </MediaQuery>
+
   </div>
 )
 
